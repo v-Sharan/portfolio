f3431e2648903dd785d4398354b5aa6033f1616d
v-Sharan <vsharansago@gmail.com>
2023-04-09 17:29:41 +0530

    second commit

--- backend/schemas/index.ts
+++ backend/schemas/index.ts
@@ -1,8 +1,6 @@
 import works from './works'
-import testimonials from './testimonials'
-import brands from './brands'
 import abouts from './abouts'
 import skills from './skills'
 import contact from './contact'
 
-export const schemaTypes = [works, testimonials, brands, abouts, skills, contact]
+export const schemaTypes = [works, abouts, skills, contact]
