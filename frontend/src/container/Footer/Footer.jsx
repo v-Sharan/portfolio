61099a2761eee575f235a800007ffb8d9304ece8
v-Sharan <vsharansago@gmail.com>
2024-01-07 22:19:59 +0530

    added email logic in frontend

--- frontend/src/container/Footer/Footer.jsx
+++ frontend/src/container/Footer/Footer.jsx
@@ -19,12 +19,13 @@ const Footer = () => {
 
   const handlePost = (data) => {
     setLoading(true);
-
-    client
-      .create({
-        _type: "contact",
-        ...data,
-      })
+    fetch("https://email-resend.onrender.com/email", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
       .then(() => {
         setLoading(false);
         setIsFormSubmitted(true);
